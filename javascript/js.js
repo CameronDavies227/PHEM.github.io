d0929efa4723f1827493e522c4d5120eccb932c7
CameronDavies227 <41108322+CameronDavies227@users.noreply.github.com>
2021-05-03 01:23:29 +1000

    validation

--- javascript/js.js
+++ javascript/js.js
@@ -1,3 +1,18 @@
-function swapBoxes() {
-    console.log('meow');
+function errorcheck(value, message) {
+    if (!value.checkValidity()) {
+        document.getElementById(message).innerHTML = value.name
+        console.log('woof');
+    } else {
+        document.getElementById(message).innerHTML = ""
+        console.log('meow');
+    }
+}
+function subjectcheck(value, message){
+    if (value==1) {
+        document.getElementById(message).innerHTML = value.name
+        console.log('woof');
+    } else {
+        document.getElementById(message).innerHTML = ""
+        console.log('meow');
+    }
 }
