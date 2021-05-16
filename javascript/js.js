3f83947820f2f0f72e41bdb4c66b27b7bd6890da
CameronDavies227 <41108322+CameronDavies227@users.noreply.github.com>
2021-05-17 01:11:20 +1000

    2

--- javascript/js.js
+++ javascript/js.js
@@ -1,6 +1,6 @@
 function errorcheck(value, message) {
     if (!value.checkValidity()) {
-        document.getElementById(message).innerHTML = value.name
+        document.getElementById(message).innerHTML = document.getElementById(message).title 
         console.log('woof');
     } else {
         document.getElementById(message).innerHTML = ""
