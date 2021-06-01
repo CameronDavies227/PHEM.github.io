1af2246c60388b775b97ed67c0d531b49cec9ff8
CameronDavies227 <41108322+CameronDavies227@users.noreply.github.com>
2021-06-01 22:08:12 +1000

    alright looks good

--- javascript/js.js
+++ javascript/js.js
@@ -16,3 +16,44 @@ function subjectcheck(value, message){
         console.log('meow');
     }
 }
+function formcheck() {
+    if (!document.getElementById("Firstname").checkValidity()) {
+        document.getElementById("Firstname").innerHTML = document.getElementById("errorFN").title 
+        console.log('woof');
+    } else {
+        document.getElementById("errorFN").innerHTML = ""
+        console.log('meow');
+    }
+
+    if (!document.getElementById("Lastname").checkValidity()) {
+        document.getElementById("Lastname").innerHTML = document.getElementById("errorLN").title 
+        console.log('woof');
+    } else {
+        document.getElementById("errorLN").innerHTML = ""
+        console.log('meow');
+    }
+
+    if (!document.getElementById("Email").checkValidity()) {
+        document.getElementById("Email").innerHTML = document.getElementById("errorE").title 
+        console.log('woof');
+    } else {
+        document.getElementById("errorE").innerHTML = ""
+        console.log('meow');
+    }
+
+    if (!document.getElementById("Phone").checkValidity()) {
+        document.getElementById("Phone").innerHTML = document.getElementById("errorP").title 
+        console.log('woof');
+    } else {
+        document.getElementById("errorP").innerHTML = ""
+        console.log('meow');
+    }
+
+    if (!document.getElementById("message-box").checkValidity()) {
+        document.getElementById("message-box").innerHTML = document.getElementById("errorM").title 
+        console.log('woof');
+    } else {
+        document.getElementById("errorM").innerHTML = ""
+        console.log('meow');
+    }
+}
